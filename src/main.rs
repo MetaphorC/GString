4b742d09a5b337f399aa5169ef4fc4a9c5dd4289
MetaphorC <ypstomar57@gmail.com>
2023-09-03 19:46:06 +0530

    Completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+
+#![allow(unused)]
+
+use clap::Parser; 
+
+// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for 
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+
 }
+
+#[derive(Debug)]
+struct CustomError(String);
+
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let path = "test.txt";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
+}
+
